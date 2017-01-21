1753f325abd3d267336e563568e836866718e25b
mkim1994 <mkim2@andrew.cmu.edu>
2017-01-20 21:32:05 -0500

    suspicion slider setup

---
+++ Meatsuit/Assets/Scripts/SuspicionMeter.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class SuspicionMeter : MonoBehaviour {
+
+	public float startingSuspicion = 0f;
+	public float currentSuspicion;
+	public Slider suspicionSlider;
+	//public Image 
+
+	public float suspicionRate;
+
+	public bool isArrested;
+
+
+	// Use this for initialization
+	void Start () {
+		currentSuspicion = startingSuspicion;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		suspicionClimb ();	
+	}
+
+	void suspicionClimb(){
+		currentSuspicion += suspicionRate;
+		suspicionSlider.value = currentSuspicion;
+
+		if (currentSuspicion >= 1f & !isArrested) {
+			Arrested ();
+		}
+	}
+
+	void Arrested(){
+		isArrested = true;
+		print (isArrested);
+	}
+}
