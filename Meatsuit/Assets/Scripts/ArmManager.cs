e7dc8b4ec541a1dd4f4ba71d2953627f760bffdc
mkim1994 <mkim2@andrew.cmu.edu>
2017-01-20 23:35:08 -0500

    simple wave check

--- Meatsuit/Assets/Scripts/ArmManager.cs
+++ Meatsuit/Assets/Scripts/ArmManager.cs
@@ -7,8 +7,13 @@ public class ArmManager : MonoBehaviour {
 	private Vector3 target;
 
 	bool waving;
+	public float waveThreshold;
 
 	UIManager uimanager;
+
+	bool armMovedLeft;
+	bool armMovedRight;
+
 
 
 	// Use this for initialization
@@ -18,21 +23,44 @@ public class ArmManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		FollowMouse ();
-		CheckWave ();
+		if(Input.GetMouseButton(0)){
+			FollowMouse ();
+			CheckWave ();
+		}
+
+		FingerKeys ();
+	}
+
+
+	void FingerKeys(){
+		
+
 	}
 
 	void FollowMouse(){
 		target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 		target = new Vector3 (target.x, target.y+3, 10);
-		//target.z = transform.position.z;
 		transform.position = target;
 	}
 
 	void CheckWave(){
 		if (waving) {
 			uimanager.currentSuspicion = 0f;
+			waving = false;
 		}
-
+		if (Input.GetAxis ("Mouse X") < -waveThreshold) {
+			armMovedLeft = true;
+			if (armMovedRight) {
+				waving = true;
+			}
+			armMovedRight = false;
+		}
+		if (Input.GetAxis ("Mouse X") > waveThreshold) {
+			armMovedRight = true;
+			if (armMovedLeft) {
+				waving = true;
+			}
+			armMovedLeft = false;
+		}
 	}
 }
