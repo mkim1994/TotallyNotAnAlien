38bd8bdb2578c0274bd573c288100d237d62714f
Mostafa Haque <93haque@gmail.com>
2017-01-22 13:53:04 -0500

    Tutorial files!

---
+++ Meatsuit/Assets/Scripts/panelSwitch.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class panelSwitch : MonoBehaviour {
+
+    public GameObject start;
+    public GameObject Menu1;
+    public GameObject Menu2;
+
+    // Use this for initialization
+    public void ChangePanel()
+    {
+        if (start.activeSelf)
+        {
+
+            Menu1.SetActive(true);
+            start.SetActive(false);
+
+        }
+
+        else if (Menu1.activeSelf)
+        {
+            Menu2.SetActive(true);
+            Menu1.SetActive(false);
+        }
+        else
+        {
+            Menu2.SetActive(false);
+            Menu1.SetActive(true);
+        }
+
+    }
+}
